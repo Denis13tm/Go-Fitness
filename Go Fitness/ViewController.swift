f2dffecc1e6338fdad096c48415cc17c95d8c1ba
Otabek Tuychiev <otawflash@gmail.com>
2023-01-11 22:05:06 +0900

    Initial Commit

---
+++ Go Fitness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Go Fitness
+//
+//  Created by Otabek Tuychiev on 11/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
